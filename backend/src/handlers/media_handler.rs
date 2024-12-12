a87eab7fa0323e1e518bc0b950b2dae49c8ce0b7
harun-wangari <harunnjenga1@gmail.com>
2024-12-12 17:06:17 +0300

    created handler to get all stored files

--- backend/src/handlers/media_handler.rs
+++ backend/src/handlers/media_handler.rs
@@ -1,6 +1,8 @@
-use sqlx::MySqlPool;
+use chrono::{Date, DateTime, TimeZone, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx:: MySqlPool;
 use tokio::{fs::File, io::AsyncWriteExt};
-use axum::extract::{ Multipart, State};
+use axum::{extract::{ Multipart, State}, http::Response, Json};
 
 use super::errors::DataError;
 
@@ -14,20 +16,21 @@ pub async fn upload_file(State(db):State<MySqlPool>,mut multipart: Multipart) ->
 
             let album = "my_pictures".to_owned();
             let file_type = "photo".to_owned();
-            sqlx::query!("insert into tb_files (name,album,type) values (?,?,?)",field_name,album,file_type)
-            .fetch_one(&db)
+            let user_id = 1;  // replace with user_id
+            sqlx::query!("insert into tb_files (name,user_id,album,file_type) values (?,?,?,?)",field_name,user_id,album,file_type)
+            .execute(&db)
             .await
             .map_err(|error|
                 match error {
                     sqlx::Error::Database(e) =>  {
                         if e.code() == Some(std::borrow::Cow::Borrowed("23000"))  {
-                                DataError::QueryError(format!("image or video with file '{}' already exists",field_name).to_string())
+                                DataError::QueryError(format!("image or video with filename '{}' already exists",field_name).to_string())
                           
                         }else{
                            DataError::QueryError("some thing went wrong while executing the query".to_owned()) 
                         }
                     },
-                    _ =>  DataError::QueryError("some thing when wrong".to_owned())     
+                    _ =>  DataError::DatabaseError(error)     
                 }
             )?;
 
@@ -37,7 +40,44 @@ pub async fn upload_file(State(db):State<MySqlPool>,mut multipart: Multipart) ->
        
 
     Ok("file(s) uploaded successfully".to_owned())
-    
-    // Err(DataError::QueryError("error".to_owned()));
 
+}
+
+
+#[derive(Deserialize, Serialize)]
+pub struct User{
+    id: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct GetFilesResponse {
+    id:u32,
+    name:String,
+    album: String,
+    file_type: String,
+    date_uploaded: String,
+}
+
+pub async fn get_all_files(State(db):State<MySqlPool>,Json(user):Json<User>) -> Result<Json<Vec<GetFilesResponse>>,DataError> {
+    let  files = sqlx::query!("SELECT id, name, album, file_type, date_uploaded FROM tb_files WHERE user_id = ?", user.id)
+    .fetch_all(&db)
+    .await
+    .map_err(|error| 
+        match error {
+            sqlx::Error::RowNotFound =>  DataError::QueryError("you have not uploaded any file".to_string()),
+            _ =>  DataError::DatabaseError(error)
+        }
+    )?
+    .into_iter()
+    .map(|file| GetFilesResponse{
+            id:file.id as u32,
+            name:file.name,
+            album: file.album,
+            file_type: file.file_type,
+            date_uploaded:file.date_uploaded.to_string(),
+        }    
+    )
+    .collect();
+
+    Ok(Json(files))
 }
